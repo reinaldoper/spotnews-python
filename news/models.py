6937f4ab7ba947980b56b433f28d9c34b9c301cf
reinaldoper <reinaldoper83@gmail.com>
2023-11-22 10:11:31 -0400

    feat: req 2

--- news/models.py
+++ news/models.py
@@ -6,3 +6,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    password = models.CharField(max_length=200, null=False)
+    role = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=200, null=False)
+  
+    def __str__(self):
+        return self.name
